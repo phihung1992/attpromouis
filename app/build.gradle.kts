206a70884c8826e6b00194a00907d02ec78340a9
phihung <phihung1992@gmail.com>
2024-01-08 15:35:29 +0700

    remove compile version

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -23,14 +23,9 @@ android {
             )
         }
     }
-    compileOptions {
-        sourceCompatibility = JavaVersion.VERSION_1_8
-        targetCompatibility = JavaVersion.VERSION_1_8
-    }
 }
 
 dependencies {
-
     implementation("androidx.appcompat:appcompat:1.6.1")
     implementation("com.google.android.material:material:1.5.0")
     implementation(project(":promouis"))
